d5a78fbb653280784afa4fd13e304334aac97f8c
Takuya Noguchi <takuya@netlab.jp>
2017-09-11 21:27:40 +0900

    Reactの設定を追加。

--- init.el
+++ init.el
@@ -795,7 +795,9 @@
 (when (require 'js2-mode nil t)
   (add-hook 'js2-mode-hook
             (lambda () (setq js2-basic-offset 2)))
-  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+  (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+  (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-jsx-mode)))
 
 
 
