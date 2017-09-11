84b4e0cc8041f6a4f93cf9f96376b038e67ef31a
Takuya Noguchi <takuya@netlab.jp>
2017-09-11 22:46:57 +0900

    インデントの設定を変更。

--- init.el
+++ init.el
@@ -75,8 +75,10 @@
 ;; .#* とかのバックアップファイルを作らない
 (setq auto-save-default nil)
 
-;; Ctrl-j と Ctrl-m の入れ替え
-(electric-indent-mode -1)
+;; インデントの設定
+(electric-indent-mode 1)
+(global-set-key (kbd "C-j") 'newline)
+(global-set-key (kbd "C-m") 'electric-newline-and-maybe-indent)
 
 ;; メニューバーを消す
 (menu-bar-mode -1)
