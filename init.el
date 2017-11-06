a81b9f50407ce70d27f5fd368d2d39f15eac5e99
Takuya Noguchi <takuya@netlab.jp>
2017-11-06 10:17:39 +0900

    view-modeの設定で必要なものをundo

--- init.el
+++ init.el
@@ -1102,7 +1102,22 @@
 
 
 ;;; view-mode
+;; 参考サイト: http://syohex.hatenablog.com/entry/20110114/1294958917
 (when (require 'view nil t)
+  ;; less like
+  (define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
+  (define-key view-mode-map (kbd "?") 'View-search-regexp-backward )
+  (define-key view-mode-map (kbd "G") 'View-goto-line-last)
+  (define-key view-mode-map (kbd "b") 'View-scroll-page-backward)
+  (define-key view-mode-map (kbd "f") 'View-scroll-page-forward)
+  ;; vi/w3m like
+  (define-key view-mode-map (kbd "h") 'backward-char)
+  (define-key view-mode-map (kbd "j") 'next-line)
+  (define-key view-mode-map (kbd "k") 'previous-line)
+  (define-key view-mode-map (kbd "l") 'forward-char)
+  (define-key view-mode-map (kbd "J") 'View-scroll-line-forward)
+  (define-key view-mode-map (kbd "K") 'View-scroll-line-backward)
+
   ;; 参考サイト: http://valvallow.blogspot.jp/2010/05/emacs-view-mode.html
   ;; 書き込み不能なファイルを view-mode で開く
   (defadvice find-file
