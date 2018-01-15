edbb2be144156717a7d866dbbd6f350a4a8deadd
Takuya Noguchi <takuya@netlab.jp>
2018-01-15 15:51:39 +0900

    見出しでtを押下したとき、ステータスをTODOに変更するように設定。

--- init.el
+++ init.el
@@ -994,6 +994,8 @@
 ;; 見出し間の移動、タスクの状態の変更を楽に行えるキーバインドを有効化
 ;; 参考サイト: https://qiita.com/takaxp/items/a5a3383d7358c58240d0
 (setq org-use-speed-commands t)
+;; 見出しで t を押下したときにタスクのステータスを 'TODO' に変更
+(add-to-list 'org-speed-commands-user '("t" org-todo "TODO"))
 ;; 見出しで d を押下したときにタスクのステータスを 'DONE' に変更
 (add-to-list 'org-speed-commands-user '("d" org-todo "DONE"))
 
