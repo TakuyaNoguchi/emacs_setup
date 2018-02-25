bba0d2bca0504e68a387169ff23b3f4eccec78d0
Takuya Noguchi <takuya@netlab.jp>
2018-02-25 11:31:37 +0900

    yasnippet の設定を追加。

--- init.el
+++ init.el
@@ -1347,10 +1347,22 @@
   (yas-global-mode 1)
 
   (eval-after-load 'yasnippet
-  '(progn
-     (define-key yas-keymap (kbd "TAB") nil)
-     (define-key yas-keymap (kbd "C-c C-n") 'yas-next-field-or-maybe-expand)
-     (define-key yas-keymap (kbd "C-c C-p") 'yas-prev-field))))
+    '(progn
+       (define-key yas-keymap (kbd "TAB") nil)
+       (define-key yas-keymap (kbd "C-c C-n") 'yas-next-field-or-maybe-expand)
+       (define-key yas-keymap (kbd "C-c C-p") 'yas-prev-field)))
+
+  (eval-after-load 'company
+    '(progn
+       (defvar company-mode/enable-yas t
+         "Enable yasnippet for all backends.")
+       (defun company-mode/backend-with-yas (backend)
+         (if (or (not company-mode/enable-yas)
+                 (and (listp backend) (member 'company-yasnippet backend)))
+             backend
+           (append (if (consp backend) backend (list backend))
+                   '(:with company-yasnippet))))
+       (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends)))))
 
 
 
