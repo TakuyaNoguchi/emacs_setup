177d9649b21edc4d3c267515fe799e984743f287
Takuya Noguchi <takuya@netlab.jp>
2018-05-21 20:05:35 +0900

    補完とopen-lineのキーバインドを入れ替え。

--- init.el
+++ init.el
@@ -412,7 +412,8 @@
 ;; ヘルプ
 (bind-key* "M-?" 'help-for-help)
 ;; 補完
-(bind-key* "C-M-o" 'hippie-expand)
+(bind-key* "C-o" 'hippie-expand)
+(bind-key* "C-M-o" 'open-line)
 (setq hippie-expand-try-functions-list
       '(try-expand-dabbrev              ; カレントバッファでdabbrev
         try-expand-dabbrev-all-buffers  ; 全てのバッファでdabbrev
