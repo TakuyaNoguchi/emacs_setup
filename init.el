178314ae9b86b81e85c75120e32facda582afe56
Takuya Noguchi <takuya@netlab.jp>
2019-03-03 14:30:32 +0900

    Gaucheの設定を削除

--- init.el
+++ init.el
@@ -1714,32 +1714,3 @@
 (package-install 'quickrun)
 (when (require 'quickrun nil t)
   (global-set-key (kbd "C-c C-c") 'quickrun-with-arg))
-
-
-
-;;; Gauche
-;; emacsでGauche
-(setq process-coding-system-alist
-      (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
-(setq scheme-program-name "/usr/bin/gosh -i")
-
-(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
-(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
-
-(defun scheme-other-window ()
-  "Run Gauche on other window"
-  (interactive)
-  (split-window-horizontally (/ (frame-width) 2))
-  (let ((buf-name (buffer-name (current-buffer))))
-    (scheme-mode)
-    (switch-to-buffer-other-window
-     (get-buffer-create "*scheme*"))
-    (run-scheme scheme-program-name)
-    (switch-to-buffer-other-window
-     (get-buffer-create buf-name))))
-
-(global-set-key (kbd "C-c G") 'scheme-other-window)
-
-(package-install 'paredit)
-(when (require 'paredit nil t)
-  (add-hook 'scheme-mode-hook #'enable-paredit-mode))
