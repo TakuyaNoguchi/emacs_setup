d5dae34728f9fcb4a1057ff49f469388f53049cc
Takuya Noguchi <takuya@netlab.jp>
2022-10-07 09:37:52 +0900

    melpa のURLを更新

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; パッケージ管理
 (when (require 'package nil t)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
   (fset 'package-desc-vers 'package--ac-desc-version)
   (package-initialize))
 
